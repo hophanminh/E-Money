4b30ec85c3bf77137abe48590aaab081e2d3c3bf
Khanh Nguyen Ho <hokhanhnguyen23@gmail.com>
2021-02-25 23:25:04 +0700

    Finish Home page (App Introduction Page)

---
+++ web/src/components/home/calendar.js
@@ -0,0 +1,70 @@
+import React from 'react';
+import { Typography, Paper } from '@material-ui/core';
+import { ViewState } from '@devexpress/dx-react-scheduler';
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+  AppointmentTooltip
+} from '@devexpress/dx-react-scheduler-material-ui';
+
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: '50px 20px 20px 30px'
+  },
+  paper: {
+    boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
+    borderRadius: '20px'
+  }
+}));
+
+const appointments = [
+  {
+    title: 'Nghỉ Tết',
+    startDate: new Date(2021, 1, 10),
+    endDate: new Date(2021, 1, 16),
+  }, {
+    title: 'Coding',
+    startDate: new Date(2021, 1, 24, 20, 30),
+    endDate: new Date(2021, 1, 26, 1),
+  }, {
+    title: 'Deadline',
+    startDate: new Date(2021, 2, 5, 20),
+    endDate: new Date(2021, 2, 5, 21),
+  }
+];
+const currentDate = Date.now();
+
+export default function Calendar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <div style={{ width: '40%', float: 'left', textAlign: 'justify', marginRight: '50px' }}>
+          <Typography style={{ color: '#172755', fontSize: '36px', fontWeight: 'bold' }}>
+            Nhắc nhở các sự kiện thu chi định kì
+          </Typography>
+          <Typography style={{ color: '#8794ba', marginTop: '20px' }}>
+            Người dùng sẽ không phải quên những chi tiêu cần thiết của mỗi tuần hoặc mỗi tháng. E-Money sẽ lên lịch nhắc nhớ người dùng trước ngày diễn ra các hoạt động này.
+          </Typography>
+        </div>
+        <div style={{ width: '60%', float: 'right' }}>
+          <Paper className={classes.paper}>
+            <Scheduler data={appointments}>
+              <ViewState currentDate={currentDate} />
+              <MonthView />
+              <Appointments />
+              <AppointmentTooltip
+                showCloseButton
+                showOpenButton
+              />
+            </Scheduler>
+          </Paper>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ web/src/components/home/introduction.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Typography, Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+import imgTemp from '../../resources/images/moneySaving.png';
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    margin: '50px 20px 20px 30px'
+  },
+  button: {
+    padding: 0,
+    borderRadius: '8px',
+    lineHeight: 'normal',
+    fontWeight: 'normal',
+    textTransform: 'none'
+  }
+}));
+
+export default function Introduction() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.container} style={{ display: 'flex', alignItems: 'center' }}>
+      <div style={{ width: '50%', float: 'left', textAlign: 'left' }}>
+        <Typography style={{ fontSize: '48px', width: '600px', marginBottom: '50px' }}>
+          Chi tiêu hiệu quả hơn bằng việc sử dụng E-Money
+        </Typography>
+        <div >
+          <Button className={classes.button}
+            style={{ height: '70px', width: '250px', fontSize: '30px', color: 'white', 
+              backgroundColor: '#1daf1a', marginRight: '30px' }}
+          >
+            Bắt đầu ngay
+          </Button>
+          <Button className={classes.button}
+            style={{ height: '70px', width: '250px', fontSize: '30px', color: 'black',
+              border: '1px solid #1daf1a' }}
+          >
+            Tìm hiểu thêm
+          </Button>
+        </div>
+      </div>
+
+      <div style={{ width: '50%', float: "right", textAlign: 'center' }}>
+        <img width={540} height={480} src={imgTemp} style={{ borderRadius: '50px' }}></img>
+      </div>
+    </div>
+  );
+}
