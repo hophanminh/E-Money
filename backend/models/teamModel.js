8fd89492a8874492ae6c709ad54b36bac48aacad
Hồ Phan Minh <38271639+Akashi195@users.noreply.github.com>
2021-03-10 00:22:23 +0700

    Create team
    Create team

---
+++ backend/models/teamModel.js
@@ -0,0 +1,15 @@
+const db = require('../utils/database');
+
+module.exports = {
+    getTeamsByUserId: (userId) => {
+        const sql = `SELECT * from teams_has_users tu WHERE tu.UserID = ${userId}`
+        return db.load(sql);
+    },
+    getTeamByWalletId: (walletId) => {
+        const sql = `SELECT * from teams t WHERE t.WalletID = ${walletId}`
+        return db.load(sql);
+    },
+    createTeam(newTeam) {
+        return db.add('teams', newTeam);
+    }
+}
