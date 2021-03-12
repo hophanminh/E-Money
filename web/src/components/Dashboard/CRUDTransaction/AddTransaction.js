a419dcee11447bf18c847f74f394b2b82ccfba37
masa23vn <48360432+masa23vn@users.noreply.github.com>
2021-03-12 21:14:48 +0700

    fix last merge

--- web/src/components/Dashboard/CRUDTransaction/AddTransaction.js
+++ web/src/components/Dashboard/CRUDTransaction/AddTransaction.js
@@ -28,7 +28,7 @@ const useStyles = makeStyles({
     fontWeight: 'bold',
     marginBottom: '-10px'
   },
-  
+
   amountRow: {
     display: 'flex',
   },
@@ -45,7 +45,7 @@ const useStyles = makeStyles({
   type2Text: {
     color: '#FF2626'
   },
-  
+
   categoryIconBox: {
     display: 'flex',
     justifyContent: 'flex-start',
@@ -103,7 +103,7 @@ export default function AddTransaction({ open, setOpen, addList }) {
     categoryName: "",
     eventName: "",
   })
-  
+
   const clearNewTransaction = () => {
     setType("Chi");
     setNewTransaction({
@@ -116,202 +116,203 @@ export default function AddTransaction({ open, setOpen, addList }) {
       categoryName: "",
       eventName: "",
     })
+  }
 
-    const handleCloseAddDialog = () => {
-      setOpen(false);
-      clearNewTransaction();
-    }
-    const handleAdd = () => {
-      const newCategory = fakeCategory.find(i => i.id === newTransaction.catID);
-      const newEvent = fakeEvent.find(i => i.id === newTransaction.eventID);
+  const handleCloseAddDialog = () => {
+    setOpen(false);
+    clearNewTransaction();
+  }
+  const handleAdd = () => {
+    const newCategory = fakeCategory.find(i => i.id === newTransaction.catID);
+    const newEvent = fakeEvent.find(i => i.id === newTransaction.eventID);
 
-      newTransaction.avatar = newCategory.avatar;
-      newTransaction.categoryName = newCategory.categoryName;
-      newTransaction.eventName = newEvent ? newEvent.name : null;
+    newTransaction.avatar = newCategory.avatar;
+    newTransaction.categoryName = newCategory.categoryName;
+    newTransaction.eventName = newEvent ? newEvent.name : null;
 
-      addList(newTransaction);
-      setOpen(false);
-      clearNewTransaction();
-    }
+    addList(newTransaction);
+    setOpen(false);
+    clearNewTransaction();
+  }
 
 
   // transaction 
 
-    const handleChangeType = (event) => {
-      setType(event.target.value);
-      setNewTransaction({
-        ...newTransaction,
-        price: newTransaction.price * (-1),
-      });
-    }
+  const handleChangeType = (event) => {
+    setType(event.target.value);
+    setNewTransaction({
+      ...newTransaction,
+      price: newTransaction.price * (-1),
+    });
+  }
 
-    const handleChange = (event) => {
-      setNewTransaction({
-        ...newTransaction,
-        [event.target.name]: event.target.value
-      });
-    }
-    const handleChangeTime = (time) => {
-      setNewTransaction({
-        ...newTransaction,
-        time: time
-      });
-    }
+  const handleChange = (event) => {
+    setNewTransaction({
+      ...newTransaction,
+      [event.target.name]: event.target.value
+    });
+  }
+  const handleChangeTime = (time) => {
+    setNewTransaction({
+      ...newTransaction,
+      time: time
+    });
+  }
 
-    const handleChangeMoney = (e) => {
-      const max = 999999999;
-      let temp = e.target.value === '' ? 0 : Number(e.target.value);
-      temp = temp > max ? max : temp;
-      temp = temp < 0 ? 0 : temp;
-      setNewTransaction({
-        ...newTransaction,
-        price: type === "Thu" ? temp : temp * -1,
-      });
-    }
+  const handleChangeMoney = (e) => {
+    const max = 999999999;
+    let temp = e.target.value === '' ? 0 : Number(e.target.value);
+    temp = temp > max ? max : temp;
+    temp = temp < 0 ? 0 : temp;
+    setNewTransaction({
+      ...newTransaction,
+      price: type === "Thu" ? temp : temp * -1,
+    });
+  }
 
-    return (
-      <Dialog open={open} onClose={handleCloseAddDialog} aria-labelledby="form-dialog-title">
-        <DialogTitle id="form-dialog-title" >
-          <Typography className={classes.title}>
-            Thêm khoản giao dịch mới
+  return (
+    <Dialog open={open} onClose={handleCloseAddDialog} aria-labelledby="form-dialog-title">
+      <DialogTitle id="form-dialog-title" >
+        <Typography className={classes.title}>
+          Thêm khoản giao dịch mới
                 </Typography>
-        </DialogTitle>
-        <DialogContent>
-          <Box>
-            <Box className={classes.amountRow}>
-              <TextField
-                className={classes.textField}
-                size="small"
-                id="isNegative"
-                name="isNegative"
-                select
-                label=""
-                value={type}
-                onChange={handleChangeType}
-                variant="outlined"
-              >
-                <MenuItem value={"Chi"}>
-                  <Box className={`${classes.typeBox} ${classes.type2Text}`}>
-                    Chi
-                </Box>
-                </MenuItem>
-                <MenuItem value={"Thu"}>
-                  <Box className={`${classes.typeBox} ${classes.type1Text}`}>
-                    Thu
-                </Box>
-                </MenuItem>
-              </TextField>
-
-              <TextField
-                className={`${classes.textField}`}
-                size="small"
-                autoFocus
-                id="price"
-                name="price"
-                label="Số tiền *"
-                type="number"
-                value={Math.abs(newTransaction.price)}
-                onChange={e => handleChangeMoney(e)}
-                InputLabelProps={{
-                  shrink: true,
-                }}
-                InputProps={{
-                  className: type === "Thu" ? classes.type1Text : classes.type2Text
-                }}
-                fullWidth
-                variant="outlined"
-              />
-            </Box>
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-              <KeyboardDateTimePicker
-                name="time"
-                size="small"
-                className={classes.textField}
-                value={newTransaction.time}
-                onChange={time => handleChangeTime(time)}
-                label="Thời gian *"
-                onError={console.log}
-                maxDate={new Date()}
-                format="dd/MM/yyyy - hh:mm a"
-                inputVariant="outlined"
-              />
-            </MuiPickersUtilsProvider>
-
+      </DialogTitle>
+      <DialogContent>
+        <Box>
+          <Box className={classes.amountRow}>
             <TextField
               className={classes.textField}
               size="small"
-              id="category"
-              name="catID"
+              id="isNegative"
+              name="isNegative"
               select
-              label="Hạng mục"
-              value={newTransaction.catID}
-              onChange={handleChange}
-              fullWidth
+              label=""
+              value={type}
+              onChange={handleChangeType}
               variant="outlined"
             >
-              {fakeCategory.map((cat) => (
-                <MenuItem key={cat.id} value={cat.id}>
-                  <Box className={classes.categoryIconBox}>
-                    <DefaultIcon
-                      avatar={cat.avatar}
-                      backgroundSize={24}
-                      iconSize={14} />
-                    <Typography className={classes.iconText}>
-                      {cat.categoryName}
-                    </Typography>
-                  </Box>
-                </MenuItem>
-              ))}
-            </TextField>
-            <TextField
-              className={classes.textField}
-              size="small"
-              id="event"
-              name="eventID"
-              select
-              label="Sự kiện"
-              value={newTransaction.eventID}
-              onChange={handleChange}
-              fullWidth
-              variant="outlined"
-            >
-              <MenuItem key={0} value={0}>
-                Không có
-                        </MenuItem>
-              {fakeEvent.map((event) => (
-                <MenuItem key={event.id} value={event.id}>
-                  {event.name}
-                </MenuItem>
-              ))}
+              <MenuItem value={"Chi"}>
+                <Box className={`${classes.typeBox} ${classes.type2Text}`}>
+                  Chi
+                </Box>
+              </MenuItem>
+              <MenuItem value={"Thu"}>
+                <Box className={`${classes.typeBox} ${classes.type1Text}`}>
+                  Thu
+                </Box>
+              </MenuItem>
             </TextField>
 
             <TextField
-              name="description"
+              className={`${classes.textField}`}
               size="small"
-              className={classes.textField}
-              value={newTransaction.description}
-              onChange={handleChange}
-              id="outlined-multiline-static"
-              label="Mô tả"
-              multiline
-              rows={10}
+              autoFocus
+              id="price"
+              name="price"
+              label="Số tiền *"
+              type="number"
+              value={Math.abs(newTransaction.price)}
+              onChange={e => handleChangeMoney(e)}
+              InputLabelProps={{
+                shrink: true,
+              }}
+              InputProps={{
+                className: type === "Thu" ? classes.type1Text : classes.type2Text
+              }}
               fullWidth
               variant="outlined"
             />
-
-            <Box className={classes.buttonBox}>
-              <Button className={`${classes.button} ${classes.closeButton}`} onClick={handleCloseAddDialog} variant="contained" >
-                Hủy
-                        </Button>
-              <Button className={`${classes.button} ${classes.addButton}`} onClick={handleAdd} variant="contained">
-                Thêm
-                        </Button>
-            </Box>
           </Box>
-        </DialogContent>
-        <DialogActions>
-        </DialogActions>
-      </Dialog>
-    );
-  }
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <KeyboardDateTimePicker
+              name="time"
+              size="small"
+              className={classes.textField}
+              value={newTransaction.time}
+              onChange={time => handleChangeTime(time)}
+              label="Thời gian *"
+              onError={console.log}
+              maxDate={new Date()}
+              format="dd/MM/yyyy - hh:mm a"
+              inputVariant="outlined"
+            />
+          </MuiPickersUtilsProvider>
+
+          <TextField
+            className={classes.textField}
+            size="small"
+            id="category"
+            name="catID"
+            select
+            label="Hạng mục"
+            value={newTransaction.catID}
+            onChange={handleChange}
+            fullWidth
+            variant="outlined"
+          >
+            {fakeCategory.map((cat) => (
+              <MenuItem key={cat.id} value={cat.id}>
+                <Box className={classes.categoryIconBox}>
+                  <DefaultIcon
+                    avatar={cat.avatar}
+                    backgroundSize={24}
+                    iconSize={14} />
+                  <Typography className={classes.iconText}>
+                    {cat.categoryName}
+                  </Typography>
+                </Box>
+              </MenuItem>
+            ))}
+          </TextField>
+          <TextField
+            className={classes.textField}
+            size="small"
+            id="event"
+            name="eventID"
+            select
+            label="Sự kiện"
+            value={newTransaction.eventID}
+            onChange={handleChange}
+            fullWidth
+            variant="outlined"
+          >
+            <MenuItem key={0} value={0}>
+              Không có
+                        </MenuItem>
+            {fakeEvent.map((event) => (
+              <MenuItem key={event.id} value={event.id}>
+                {event.name}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <TextField
+            name="description"
+            size="small"
+            className={classes.textField}
+            value={newTransaction.description}
+            onChange={handleChange}
+            id="outlined-multiline-static"
+            label="Mô tả"
+            multiline
+            rows={10}
+            fullWidth
+            variant="outlined"
+          />
+
+          <Box className={classes.buttonBox}>
+            <Button className={`${classes.button} ${classes.closeButton}`} onClick={handleCloseAddDialog} variant="contained" >
+              Hủy
+                        </Button>
+            <Button className={`${classes.button} ${classes.addButton}`} onClick={handleAdd} variant="contained">
+              Thêm
+                        </Button>
+          </Box>
+        </Box>
+      </DialogContent>
+      <DialogActions>
+      </DialogActions>
+    </Dialog>
+  );
+}
 
